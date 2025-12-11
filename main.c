ff1ad51ddd42eeea471396215ff128bcc029d904
Lyvan09 <nyalyvan2@gmail.com>
2025-12-11 09:12:04 +0100

    11/12/2025 (M.A.J esthétique)

--- main.c
+++ main.c
@@ -4,19 +4,22 @@
 
 //main function to test fact and power
 int main( void ) { 
+printf("Main function to test fact and power\n");
 int result = power( 2, 3 ); 
 printf( "2^3 == %d\n", result ); 
 result = fact( 6 ); 
 printf( "6! == %d\n", result ); 
+
+printf("*****************************\n");
+
+//Main function to test permutation
+printf("Main function to test permutation\n");
+int first = 16 ;
+int second = 20;
+printf("first = %d || second = %d \n" , first, second);
+permutation( &first, &second);
+printf("first = %d || second = %d \n" , first, second);
+
 return EXIT_SUCCESS; 
 } 
--   
-//Main function to test permutation
-int main(){ 
-    int first = 16 ;
-    int second = 20;
-    printf(" first = %d \n second = %d \n" , first, second);
-    permutation( &first, &second);
-    printf(" first = %d \n second = %d" , first, second);
-    return 0;
-}
+   
